2afccfc5ea80299ee4fd1fa95d069637717cec3d
Md. Mahadhi Hassan Chowdhury <mhcsunny@gmail.com>
2021-09-06 10:32:54 +0600

    Initial Commit

---
+++ SwiftUIStanford/SwiftUIStanfordApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStanfordApp.swift
+//  SwiftUIStanford
+//
+//  Created by Workspace Infotech on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStanfordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
